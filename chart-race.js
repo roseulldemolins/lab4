cd223f574f995487cc2f1bfb256550d896957aa4
Rose <rose.r.ulldemolins@lloydsbanking.com>
2022-04-04 23:36:06 +0100

    bar chart working

--- chart-race.js
+++ chart-race.js
@@ -1,6 +1,6 @@
 function drawChartRace(){
 
-let companyData = "http://localhost:8000/category-brands.csv"
+let companyData = "http://localhost:8000/testing.csv"
 
 var svg = d3.select("#bar_race")
 .attr("width", 960)
@@ -8,9 +8,9 @@ var svg = d3.select("#bar_race")
 
 
 
-var tickDuration = 500;
+var tickDuration = 2000;
 
-var top_n = 12;
+var top_n = 5;
 var height = 600;
 var width = 960;
 
@@ -40,26 +40,34 @@ let caption = svg.append('text')
 .style('text-anchor', 'end')
 .html('Source: Interbrand');
 
-let year = 2000;
+let year = 2015;
 
 d3.csv(companyData).then(function(data) {
 //if (error) throw error;
 
 console.log(data);
 
+const companies = ["Google", "Microsoft", "Facebook", "Apple", "Amazon"]
+// const companies = ["Google"]
+data = data.filter(
+    (line) =>
+      companies.includes(line.Company)
+  );
+
  data.forEach(d => {
   d.value = +d.value,
-  d.lastValue = +d.lastValue,
+  d.lastValue =  5,
   d.value = isNaN(d.value) ? 0 : d.value,
-  d.date = +d.date,
+  d.year = +d.year,
   d.colour = d3.hsl(Math.random()*360,0.75,0.75)
 });
 
 console.log(data);
 
-let yearSlice = data.filter(d => d.year == year && !isNaN(d.value))
+let yearSlice = data.filter(d => 
+    d.year == year && !isNaN(d.value))
 .sort((a,b) => b.value - a.value)
-.slice(0, top_n);
+.slice(0, 5);
 
 yearSlice.forEach((d,i) => d.rank = i);
 
@@ -69,8 +77,10 @@ let x = d3.scaleLinear()
   .domain([0, d3.max(yearSlice, d => d.value)])
   .range([margin.left, width-margin.right-65]);
 
+
+
 let y = d3.scaleLinear()
-  .domain([top_n, 0])
+  .domain([5, 0])
   .range([height-margin.bottom, margin.top]);
 
 let xAxis = d3.axisTop()
@@ -84,10 +94,10 @@ svg.append('g')
  .attr('transform', `translate(0, ${margin.top})`)
  .call(xAxis)
  .selectAll('.tick line')
- .classed('origin', d => d == 0);
+//  .classed('origin', d => d == 0);
 
 svg.selectAll('rect.bar')
-  .data(yearSlice, d => d.name)
+  .data(yearSlice, d => d.Company)
   .enter()
   .append('rect')
   .attr('class', 'bar')
@@ -95,20 +105,20 @@ svg.selectAll('rect.bar')
   .attr('width', d => x(d.value)-x(0)-1)
   .attr('y', d => y(d.rank)+5)
   .attr('height', y(1)-y(0)-barPadding)
-  .style('fill', d => d.colour);
+  .style('fill', d => console.log(x(d.value)-x(0)-1));
 
 svg.selectAll('text.label')
-  .data(yearSlice, d => d.name)
+  .data(yearSlice, d => d.Company)
   .enter()
   .append('text')
   .attr('class', 'label')
   .attr('x', d => x(d.value)-8)
   .attr('y', d => y(d.rank)+5+((y(1)-y(0))/2)+1)
   .style('text-anchor', 'end')
-  .html(d => d.name);
+  .html(d => d.Company);
 
 svg.selectAll('text.valueLabel')
-.data(yearSlice, d => d.name)
+.data(yearSlice, d => d.Company)
 .enter()
 .append('text')
 .attr('class', 'valueLabel')
@@ -126,13 +136,18 @@ let yearText = svg.append('text')
 
 let ticker = d3.interval(e => {
 
+
 yearSlice = data.filter(d => d.year == year && !isNaN(d.value))
   .sort((a,b) => b.value - a.value)
   .slice(0,top_n);
 
+  console.log(data)
+
+  console.log('IntervalYear: ', yearSlice);
+
 yearSlice.forEach((d,i) => d.rank = i);
 
-//console.log('IntervalYear: ', yearSlice);
+console.log('IntervalYear: ', yearSlice);
 
 x.domain([0, d3.max(yearSlice, d => d.value)]); 
 
@@ -142,12 +157,12 @@ svg.select('.xAxis')
   .ease(d3.easeLinear)
   .call(xAxis);
 
- let bars = svg.selectAll('.bar').data(yearSlice, d => d.name);
+ let bars = svg.selectAll('.bar').data(yearSlice, d => d.Company);
 
  bars
   .enter()
   .append('rect')
-  .attr('class', d => `bar ${d.name.replace(/\s/g,'_')}`)
+  .attr('class', d => `bar ${d.Company.replace(/\s/g,'_')}`)
   .attr('x', x(0)+1)
   .attr( 'width', d => x(d.value)-x(0)-1)
   .attr('y', d => y(top_n+1)+5)
@@ -175,7 +190,7 @@ svg.select('.xAxis')
     .remove();
 
  let labels = svg.selectAll('.label')
-    .data(yearSlice, d => d.name);
+    .data(yearSlice, d => d.Company);
 
  labels
   .enter()
@@ -184,7 +199,7 @@ svg.select('.xAxis')
   .attr('x', d => x(d.value)-8)
   .attr('y', d => y(top_n+1)+5+((y(1)-y(0))/2))
   .style('text-anchor', 'end')
-  .html(d => d.name)    
+  .html(d => d.Company)    
   .transition()
     .duration(tickDuration)
     .ease(d3.easeLinear)
@@ -209,7 +224,7 @@ svg.select('.xAxis')
    
 
 
- let valueLabels = svg.selectAll('.valueLabel').data(yearSlice, d => d.name);
+ let valueLabels = svg.selectAll('.valueLabel').data(yearSlice, d => d.Company);
 
  valueLabels
     .enter()
@@ -249,7 +264,7 @@ valueLabels
 yearText.html(~~year);
 
 if(year == 2018) ticker.stop();
-year = d3.format('.1f')((+year) + 0.1);
+year = d3.format('.1f')((+year) + 1);
 },tickDuration);
 
 });
