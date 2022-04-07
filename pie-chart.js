421f8cfca6226fcd48dcba292f01a6ebc2b8cf6b
Rose <rose.r.ulldemolins@lloydsbanking.com>
2022-04-07 21:30:31 +0100

    paths added

--- pie-chart.js
+++ pie-chart.js
@@ -1,108 +1,37 @@
-function removePieChart(country) {
-  d3.select("#pie_chart_svg").selectAll("*").remove();
-  pieChart(country);
-}
+function drawPieChart(){
 
-function pieChart(chosenContinent) {
-  // File that includes all the covid vaccinations based on country
-  let latest = "http://localhost:8000/latest-world-deaths.csv";
+var dataset = {
+  apples: [53245, 28479, 19697, 24037, 40245],
+  oranges: [53245, 28479, 19697, 24037, 40245],
+  lemons: [53245, 28479, 19697, 24037, 40245],
+  pears: [53245, 28479, 19697, 24037, 40245],
+  pineapples: [53245, 28479, 19697, 24037, 4045],
+};
 
-  // Setting the size and margins of the graph
-  var margin = { top: 250, right: 30, bottom: 30, left: 250 },
-    width = 500 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-  // Adding an svg to the page, and applying the dimensions above
-  var svg = d3
-    .select("#pie_chart_svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+var width = 460,
+    height = 300,
+    cwidth = 25;
+var color = 'black'
+var pie = d3.pie()
+    .sort(null);
+var arc = d3.arc();
+var svg = d3.select("#pie_chart")
+    .attr("width", width)
+    .attr("height", height)
     .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  //Read the data
-  d3.csv(latest).then(function (data) {
-    if (chosenContinent !== "All") {
-      data = data.filter((country) => country.continent == chosenContinent);
-    } else {
-      data = data.filter((country) => country.continent !== "");
-    }
-    var width = 400,
-      height = 400,
-      radius = Math.min(width, height) / 2;
-
-    // Compute the position of each group on the pie:
-    const pie = d3.pie().value(function (d) {
-      return d.total_deaths;
-    });
-    const data_ready = pie(data);
-
-    var arc = d3
-      .arc()
-      .innerRadius(radius - 100)
-      .outerRadius(radius - 20);
-
-    // set the color scale
-    const color = d3
-      .scaleSequential()
-      .domain([1, data.length])
-      .interpolator(d3.interpolatePuRd);
-
-    // shape helper to build arcs:
-    const arcGenerator = d3.arc().innerRadius(50).outerRadius(radius);
-
-    // Here we create a div for the tooltip with styling
-    var tooltip = d3
-      .select("body")
-      .append("div")
-      .attr("class", "tooltip")
-      .style("border", "solid")
-      .style("border-width", "2px")
-      .style("opacity", 0);
-
-    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-    svg
-      .selectAll("mySlices")
-      .data(data_ready)
-      .join("path")
-      .attr("d", arcGenerator)
-      .attr("fill", function (d, i) {
-        return color(i);
-      })
-      .attr("stroke", "black")
-      .style("stroke-width", "2px")
-      .style("opacity", 1)
-      .on("mouseover", function (event, d) {
-        console.log(d);
-        tooltip.transition().duration(200).style("opacity", 0.9);
-        tooltip
-          .html(
-            "<strong>Country: </strong><span class='details'>" +
-              d.data.location +
-              "<br></span>" +
-              "<strong>Total Deaths: </strong><span class='details'>" +
-              +d.data.total_deaths +
-              "<br></span>"
-          )
-          .style("left", event.pageX + "px")
-          .style("top", event.pageY - 28 + "px");
-      })
-      .on("mouseout", function (d) {
-        tooltip.transition().duration(500).style("opacity", 0);
-      })
-      .on("click", function (event, d) {
-        removeLineChart(d.data.location);
-      })
-      // Sets a transition for 1 second
-      .transition()
-      .duration(1000)
-      // Calculating start and end angles
-      .attrTween("d", function (d) {
-        var i = d3.interpolate(d.endAngle, d.startAngle);
-        return function (t) {
-          d.startAngle = i(t);
-          return arc(d);
-        };
-      });
-  });
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+var gs = svg.selectAll("g").data(dataset).enter().append("g");
+var path = gs.selectAll("path")
+    .data(function(d) { return 
+      pie(d); })
+  .enter().append("path").transition().duration(750).attrTween("d", arcTween)
+    .attr("fill", function(d, i) { return 'black'; })
+    .attr("d", function(d, i, j) { return arc.innerRadius(10+cwidth*j).outerRadius(cwidth*(j+1))(d); });
+function arcTween(a) {
+  var i = d3.interpolate(this._current, a);
+  this._current = i(0);
+  return function(t) {
+    return arc(i(t));
+  };
+}
 }
--- stacked-bar.js
+++ stacked-bar.js
@@ -1,110 +1,183 @@
 let diversityData = "http://localhost:8000/testing.csv";
 let companyData = "http://localhost:8000/category-brands-2.csv";
 
-function drawStackedChart(){
-
-// set the dimensions and margins of the graph
-const margin = {top: 10, right: 30, bottom: 75, left: 50},
+function drawStackedChart() {
+  // set the dimensions and margins of the graph
+  const margin = { top: 10, right: 30, bottom: 85, left: 60 },
     width = 600 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
-const svg = d3.select("#stacked_svg")
+  // append the svg object to the body of the page
+  const svg = d3
+    .select("#stacked_svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+    .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-// Parse the Data
-Promise.all([
-  d3.csv(diversityData),
-  d3.csv(companyData)])
-  .then ( function(data) {
+  // Parse the Data
+  Promise.all([d3.csv(diversityData), d3.csv(companyData)]).then(function (
+    data
+  ) {
+    console.log(data);
 
-    console.log(data)
+    let diversityData = data[0];
+    let companyData = data[1];
 
-  let diversityData = data[0]
-  let companyData = data[1]
+    const companies = companyData.filter((line) => line.date.includes("2018"));
 
-  const companies = companyData.map(d => d.name)
+    const columns = diversityData.columns;
 
-  console.log(companies)
+    const companyNames = companies.map((line) => line.name);
 
-  // List of subgroups = header of the csv files = soil condition here
-  const subgroups = diversityData.columns.slice(6,13)
+    diversityData = diversityData.filter(
+      (line) => line.year == "2018" && companyNames.includes(line.Company)
+    );
 
-  console.log(subgroups)
+    diversityData.forEach((d) => {
+      d.earnings = getProfit(d.Company);
+    });
 
-  diversityData = diversityData.filter(
-    (line) =>
-      line.year == "2018" &&
-      companies.includes(line.Company)
-  );
+    function getProfit(company) {
+      return companies.filter((line) => {
+        return line.name == company;
+      })[0].value;
+    }
 
+    // List of subgroups = header of the csv files = soil condition here
+    const subgroups = columns.slice(6, 13);
 
+    // List of groups = species here = value of the first column called group -> I show them on the X axis
+    const groups = diversityData.map((d) => d.Company);
 
-  // List of groups = species here = value of the first column called group -> I show them on the X axis
-  const groups = diversityData.map(d => d.Company)
+    // Add X axis
+    const x = d3.scaleBand().domain(groups).range([0, width]).padding([0.2]);
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x).tickSizeOuter(0));
 
-  // Add X axis
-  const x = d3.scaleBand()
-      .domain(groups)
-      .range([0, width])
-      .padding([0.2])
-  svg.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x).tickSizeOuter(0));
-  
     svg
       .selectAll("text")
       .attr("transform", "rotate(-65)")
       .style("text-anchor", "end");
 
-  // Add Y axis
-  const y = d3.scaleLinear()
-    .domain([0, 100])
-    .range([ height, 0 ]);
-  svg.append("g")
-    .call(d3.axisLeft(y));
 
+  // text label for the x axis
+  svg.append("text")             
+  .attr("transform",
+        "translate(" + (width/2) + " ," + 
+                       (height + margin.top + 70) + ")")
+  .style("text-anchor", "middle")
+  .text("Technology/Social Media Company");
+
+
+    // Add Y axis
+    const y = d3.scaleLinear().domain([0, 100]).range([height, 0]);
+    svg.append("g").call(d3.axisLeft(y));
+
+      // text label for the y axis
+  svg.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 0 - margin.left)
+  .attr("x",0 - (height / 2))
+  .attr("dy", "1em")
+  .style("text-anchor", "middle")
+  .text("% of Ethnicity");  
 
     // Creating the colour palette
     const color = d3
       .scaleOrdinal()
       .range([
-        "#d60202",
-        "#a248cf",
-        "#ed9fda",
-        "#5e0551",
-        "#5e0517",
-        "#de284d",
+        "#38071C",
+        "#E9C7D6",
+        "#C0B4BE",
+        "#858187",
+        "#7B757D",
+        "#544A54",
       ]);
 
-  //stack the data? --> stack per subgroup
-  const stackedData = d3.stack()
-    .keys(subgroups)
-    (diversityData)
+    //stack the data? --> stack per subgroup
+    const stackedData = d3.stack().keys(subgroups)(diversityData);
 
-    console.log(stackedData, subgroups)
+    console.log(stackedData, subgroups);
 
-  // Show the bars
-  svg.append("g")
-    .selectAll("g")
-    // Enter in the stack data = loop key per key = group per group
-    .data(stackedData)
-    .join("g")
-      .attr("fill", d => color(d.key))
+    // Show the bars
+    svg
+      .append("g")
+      .selectAll("g")
+      // Enter in the stack data = loop key per key = group per group
+      .data(stackedData)
+      .join("g")
+      .attr("fill", (d) => color(d.key))
       .selectAll("rect")
       // enter a second time = loop subgroup per subgroup to add all rectangles
-      .data(d => d)
+      .data((d) => d)
       .join("rect")
-        .attr("x", d => 
-      // console.log(d.data.Company)
+      .attr("x", (d) =>
+        // console.log(d.data.Company)
         x(d.data.Company)
-        )
-      
-        .attr("y", d => y(d[1]))
-        .attr("height", d => y(d[0]) - y(d[1]))
-        .attr("width",x.bandwidth())
-})
+      )
+      .attr("y", (d) => y(d[1]))
+      .attr("height", (d) => y(d[0]) - y(d[1]))
+      .attr("width", x.bandwidth())
+      .on("mouseover", mouseover)
+      .on("mousemove", mousemove)
+      .on("mouseleave", mouseleave);
+
+    // var bar_enter = svg.selectAll("g")
+    // .data(stackedData)
+    // .selectAll("rect")
+    // .data(d => d)
+    // .enter();
+
+    // bar_enter.append("text")
+    // .text(function(d) {
+    //   return d3.format(".2s")(d.data.value)+"%"; })
+    // .attr("y",  d => y(d[1]))
+    // .attr("x",  d =>   x(d.data.Company)
+
+    // )
+    // .style("fill", 'black');
+  });
+
+  // Adding a tooltip (not visible yet)
+  var tooltip = d3
+    .select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("opacity", 0);
+
+  // Three function that change the tooltip when user hover / move / leave a cell
+  const mouseover = function (event, d) {
+    console.log(d);
+    const subgroupName = d3.select(this.parentNode).datum().key;
+    const subgroupValue = d.data[subgroupName];
+    tooltip
+      .html(
+        "Ethnicity: " +
+          subgroupName +
+          "<br>" +
+          "Value: " +
+          subgroupValue +
+          "%" +
+          "<br>" +
+          d.data.Company +
+          " earned $" +
+          d3.format(",d")(d.data.earnings) +
+          "m in 2018"
+      )
+      .style("opacity", 1);
+  };
+  const mousemove = function (event, d) {
+    tooltip
+      .style("transform", "translateY(-55%)")
+      .style("left", event.x + "px")
+      .style("top", event.y + "px");
+  };
+  const mouseleave = function (event, d) {
+    tooltip.style("opacity", 0);
+  };
 }
