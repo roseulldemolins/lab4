cfabe72e0d8c95653884d55788ea73184a261f11
Rose <rose.r.ulldemolins@lloydsbanking.com>
2022-04-15 02:07:25 +0100

    tooltips for heatmap working

--- heat-map.js
+++ heat-map.js
@@ -1,188 +1,199 @@
+// CSV file to read in
 let jobData = "http://localhost:8000/job_types.csv";
 
+// Function to draw overall heatmap
+function drawHeatMap() {
+  // Setting the dimensions and margins of the graph
+  const margin = { top: 40, right: 25, bottom: 300, left: 240 },
+    width = 900 - margin.left - margin.right,
+    height = 650 - margin.top - margin.bottom;
 
+  // Selecting the svg object and applying dimencions
+  var svg = d3
+    .select("#heat_map")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-function drawHeatMap(){
+  //Read the data
+  d3.csv(jobData).then(function (data) {
+    // Make a set of all the comapnies to choose from
+    const companies = Array.from(new Set(data.map((d) => d.company)));
 
+    //  Add these companies to the dropdown
+    d3.select("#selectButtonHeatMap")
+      .selectAll("myOptions")
+      .data(companies)
+      .enter()
+      .append("option")
+      // Label in menu is the same as the value
+      .text(function (d) {
+        return d;
+      })
+      .attr("value", function (d) {
+        return d;
+      });
 
-// set the dimensions and margins of the graph
-const margin = {top: 80, right: 25, bottom: 250, left: 250},
-width = 900 - margin.left - margin.right,
-height = 650 - margin.top - margin.bottom;
+    // Maps the data
+    data = data.map((line) => {
+      return {
+        company: line.company,
+        // Concatenating the gender and race
+        demographic: line.gender + " " + line.race,
+        job_category: line.job_category,
+        count: line.count,
+      };
+    });
 
-// append the svg object to the body of the page
-var svg = d3.select("#heat_map")
-.attr("width", width + margin.left + margin.right)
-.attr("height", height + margin.top + margin.bottom)
-.append("g")
-.attr("transform", `translate(${margin.left}, ${margin.top})`);
+    // Creates a set of the groups so they're unique
+    const myGroups = Array.from(new Set(data.map((d) => d.demographic)))
+      // Arranging them alphabetically
+      .sort()
+      // Removing the total columns
+      .filter((item) => !item.includes("otals"));
+    // Creating a set of all of the job types
+    const myVars = Array.from(new Set(data.map((d) => d.job_category))).filter(
+      // Filtering out totals again
+      (item) => !item.includes("otals")
+    );
 
-//Read the data
-d3.csv(jobData).then(function(data) {
+    // Creating X scales and axis
+    const x = d3.scaleBand().range([0, width]).domain(myGroups).padding(0.05);
 
-  const companies = Array.from(new Set(data.map(d => d.company)))
+    // Adding titles to heatmap
+    svg
+      .append("g")
+      .style("font-size", 10)
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x).tickSize(0))
+      .select(".domain")
+      .remove();
 
-  console.log('companies', companies)
-    // add the options to the button
-d3.select("#selectButtonHeatMap")
-.selectAll("myOptions")
-.data(companies)
-.enter()
-.append("option")
-.text(function (d) {
-  return d;
-}) // text showed in the menu
-.attr("value", function (d) {
-  return d;
-}); // corresponding value returned by the button
-data = data.map((line) => {
-  return {
-  company: line.company,
-  demographic: line.gender + " " + line.race,
-  job_category: line.job_category,
-  count: line.count
-  }
-})
+    // Rotating them so they fit better
+    svg
+      .selectAll("text")
+      .attr("transform", "rotate(-65)")
+      .style("text-anchor", "end");
 
-// Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
-const myGroups = Array.from(new Set(data.map(d => d.demographic))).sort().filter(item => !item.includes("otals"))
-const myVars = Array.from(new Set(data.map(d => d.job_category))).filter(item => !item.includes("otals"))
+    // Creating Y axis and scale
+    const y = d3.scaleBand().range([height, 0]).domain(myVars).padding(0.05);
+    // Adding the labels for job types
+    svg.append("g").style("font-size", 15).call(d3.axisLeft(y).tickSize(0));
 
-// Build X scales and axis:
-const x = d3.scaleBand()
-.range([ 0, width ])
-.domain(myGroups)
-.padding(0.05);
-svg.append("g")
-.style("font-size", 10)
-.attr("transform", `translate(0, ${height})`)
-.call(d3.axisBottom(x).tickSize(0))
-// .attr("transform", "rotate(-65)")
-// .style("text-anchor", "end")
-.select(".domain").remove()
+    // Build color scale
+    var myColor = d3
+      .scaleSequential()
+      // Sets the max to be for this partivular company
+      .domain([0, d3.max(data, (d) => +d.count)])
+      .interpolator(d3.interpolatePuRd);
 
-svg.   selectAll("text")
-.attr("transform", "rotate(-65)")
-.style("text-anchor", "end");
-
-// Build Y scales and axis:
-const y = d3.scaleBand()
-.range([ height, 0 ])
-.domain(myVars)
-.padding(0.05);
-svg.append("g")
-.style("font-size", 15)
-.call(d3.axisLeft(y).tickSize(0))
-// .select(".domain").remove()
-
- // Build color scale
- var myColor = 
- // d3.scaleLinear()
- d3.scaleSequential()
- .domain([0, d3.max(data, d => +d.count)])
- // .range(["#e9c7d6", "#38071c"])
- .interpolator(d3.interpolatePuRd);
-
-  // add the squares
-  svg.selectAll(".tile")
-    .data(data, function(d) {return d.demographic+':'+d.job_category;})
-    .enter()
-    .append("rect")
-    .attr("class", "tile")
-      .attr("x", function(d) { return x(d.demographic) })
-      .attr("y", function(d) { return y(d.job_category) })
+    // Adding the squares
+    svg
+      .selectAll(".tile")
+      // Unique name
+      .data(data, function (d) {
+        return d.demographic + ":" + d.job_category;
+      })
+      .enter()
+      .append("rect")
+      .attr("class", "tile")
+      // X coord based on demographic
+      .attr("x", function (d) {
+        return x(d.demographic);
+      })
+      // Y coord based on job type
+      .attr("y", function (d) {
+        return y(d.job_category);
+      })
+      // Rounding corners off
       .attr("rx", 4)
       .attr("ry", 4)
-      .attr("width", x.bandwidth() )
-      .attr("height", y.bandwidth() )
-      .style("fill", function(d) { return myColor(d.count)} )
-      .style("stroke-width", 4)
-      .style("stroke", "none")
-      .style("opacity", 0.8)
-
-    // When the button is changed, run the updateChart function
-    d3.select("#selectButtonHeatMap").on("change", function(d) {
-            // recover the option that has been chosen
-            var company = d3.select(this).property("value")
-      // run the updateChart function with this selected option
-      updateHeatMap(company, data)
-  })
-
-})}
-
-function updateHeatMap(company, data) {
-
-  // Here we filter it to the chosen employer
-  data = data.filter((line) => line.company == company && !line.demographic.includes("otals") && !line.job_category.includes("otals"));
-
-
-       
-
-  // Build color scale
-  var myColor = 
-  // d3.scaleLinear()
-  d3.scaleSequential()
-  .domain([0, d3.max(data, d => 
-    // console.log(d.count)
-    +d.count
-    
-    )])
-  // .range(["#e9c7d6", "#38071c"])
-  .interpolator(d3.interpolatePuRd);
-
-  // create a tooltip
-  const tooltip = d3.select("#heat_map")
-    .append("div")
-    .style("opacity", 0)
-    .attr("class", "tooltip")
-    .style("background-color", "white")
-    .style("border", "solid")
-    .style("border-width", "2px")
-    .style("border-radius", "5px")
-    .style("padding", "5px")
-
-  // Three function that change the tooltip when user hover / move / leave a cell
-  const mouseover = function(event,d) {
-    console.log(d.count)
-    tooltip
-      .style("opacity", 1)
-    d3.select(this)
-      .style("stroke", "black")
-      .style("opacity", 1)
-  }
-  const mousemove = function(event,d) {
-    tooltip
-      .html("The exact value of<br>this cell is: " + d.count)
-      .style("left", (event.x)/2 + "px")
-      .style("top", (event.y)/2 + "px")
-  }
-  const mouseleave = function(event,d) {
-    tooltip
-      .style("opacity", 0)
-    d3.select(this)
-      .style("stroke", "none")
-      .style("opacity", 0.8)
-  }
-
-  var svg = d3.select("#heat_map")
-  // add the squares
-  svg
-  .selectAll(".tile")
-
-    .data(data, function(d) {return d.demographic+':'+d.job_category;})
-    .join("rect")
-
-    .transition()
-    .duration(500)
-      .style("fill", function(d) { 
-        return myColor(d.count)
+      // Evenly sizes for x and y
+      .attr("width", x.bandwidth())
+      .attr("height", y.bandwidth())
+      .style("fill", function (d) {
+        // Colour is based on volume of people
+        return myColor(d.count);
       })
       .style("stroke-width", 4)
       .style("stroke", "none")
-      .style("opacity", 0.8)
+      .style("opacity", 0.8);
+
+    // When the button is changed, run the updateChart function
+    d3.select("#selectButtonHeatMap").on("change", function (d) {
+      // Get the option chosen by the user
+      var company = d3.select(this).property("value");
+      // Call update chart function with this comapny
+      updateHeatMap(company, data);
+    });
+  });
+}
+
+// Just to update colour rather than redraw
+function updateHeatMap(company, data) {
+  // Adding a tooltip which won't be visible yet
+  var tooltip = d3
+    .select("#heat_map_div")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("opacity", 0);
+
+  // Here we filter it to the chosen employer
+  data = data.filter(
+    (line) =>
+      line.company == company &&
+      !line.demographic.includes("otals") &&
+      !line.job_category.includes("otals")
+  );
+
+  // Build color scale again so it's relevant to this data
+  var myColor = d3
+    .scaleSequential()
+    .domain([0, d3.max(data, (d) => +d.count)])
+    .interpolator(d3.interpolatePuRd);
+
+  // Shows the tooltip and sets the content when the mouse moves over a tile
+  const mouseover = function (event, d) {
+    tooltip.style("opacity", 1).html("There are " + d.count + " people.");
+    d3.select(this).style("stroke", "black").style("opacity", 1);
+  };
+
+  // Moves the tooltip with the mouse
+  const mousemove = function (event, d) {
+    tooltip
+      .style("left", event.x - 100 + "px")
+      .style("top", event.y + 100 + "px");
+  };
+
+  // Hides teh tooltip when the mouse leaves
+  const mouseleave = function (event, d) {
+    tooltip.style("opacity", 0);
+    d3.select(this).style("stroke", "none").style("opacity", 0.8);
+  };
+
+  var svg = d3.select("#heat_map");
+  // Adding in the tiles
+  svg
+    .selectAll(".tile")
+    // Attaching the data
+    .data(data, function (d) {
+      return d.demographic + ":" + d.job_category;
+    })
+    .join("rect")
+    // Adding mouse listeners
     .on("mouseover", mouseover)
     .on("mousemove", mousemove)
     .on("mouseleave", mouseleave)
-
-  
+    // Adding the transition to next colour
+    .transition()
+    .duration(500)
+    .style("fill", function (d) {
+      return myColor(d.count);
+    })
+    .style("stroke-width", 4)
+    .style("stroke", "none")
+    .style("opacity", 0.8);
 }
--- pie-chart.js
+++ pie-chart.js
@@ -1,108 +1,114 @@
+// csv file to read in
 const payGapData = "http://localhost:8000/pay-gap-2017.csv";
 
-  const width = 600,
-    height = 500,
-    chartRadius = height / 2 - 40;
+// Setting height, width and radius of chart
+const width = 600,
+  height = 500,
+  chartRadius = height / 2 - 40;
 
-  // Creating the colour palette
-  const color = d3
-    .scaleOrdinal()
-    .range(["#38071C", "#E9C7D6", "#C0B4BE", "#858187"]);
+// Creating the colour palette
+const color = d3
+  .scaleOrdinal()
+  .range(["#38071C", "#E9C7D6", "#C0B4BE", "#858187"]);
 
-  let svg = d3
-    .select("#pie_chart")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+// Applyling the above to the svg
+let svg = d3
+  .select("#pie_chart")
+  .attr("width", width)
+  .attr("height", height)
+  .append("g")
+  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-  let tooltip = d3.select("body").append("div").attr("class", "tooltip");
+  // Creating the tooltip
+let tooltip = d3.select("body").append("div").attr("class", "tooltip");
 
-  const PI = Math.PI,
-    // Sets the minimum radius for the inner arc (greater will create bigger arcs)
-    arcMinRadius = 10,
-    // Sets the space between each arc
-    arcPadding = 10,
-    // Moves the labels for the arcs left slightly (so they don't overlap the arc)
-    labelPadding = -5,
-    // The amount of segments the arc is split into
-    numTicks = 10;
+// Uses value of pi
+const PI = Math.PI,
+  // Sets the minimum radius for the inner arc (greater will create bigger arcs)
+  arcMinRadius = 10,
+  // Sets the space between each arc
+  arcPadding = 10,
+  // Moves the labels for the arcs left slightly (so they don't overlap the arc)
+  labelPadding = -5,
+  // The amount of segments the arc is split into
+  numTicks = 10;
 
-  // Get names of cereals, for dropdown
-  const companies = [
-    "GB Group PLC",
-    "COMPUTACENTER (UK) LIMITED",
-    "Softcat PLC",
-    "RENISHAW P L C",
-    "RIGHTMOVE GROUP LIMITED",
-    "AUTO TRADER LIMITED",
-    "Sage (UK) Ltd",
-    "AVEVA SOLUTIONS LIMITED",
-    "BT FACILITIES SERVICES LIMITED",
-    "VODAFONE LIMITED",
-  ];
+// Get list of companies for dropdown (from external top 10 source)
+const companies = [
+  "GB Group PLC",
+  "COMPUTACENTER (UK) LIMITED",
+  "Softcat PLC",
+  "RENISHAW P L C",
+  "RIGHTMOVE GROUP LIMITED",
+  "AUTO TRADER LIMITED",
+  "Sage (UK) Ltd",
+  "AVEVA SOLUTIONS LIMITED",
+  "BT FACILITIES SERVICES LIMITED",
+  "VODAFONE LIMITED",
+];
 
-  // add the options to the button
-  d3.select("#selectButton")
-    .selectAll("myOptions")
-    .data(companies)
-    .enter()
-    .append("option")
-    .text(function (d) {
-      return d;
-    }) // text showed in the menu
-    .attr("value", function (d) {
-      return d;
-    }); // corresponding value returned by the button
+// Adding the options to the dropdown
+d3.select("#selectButton")
+  .selectAll("myOptions")
+  .data(companies)
+  .enter()
+  .append("option")
+  // Value and label are the same
+  .text(function (d) {
+    return d;
+  })
+  .attr("value", function (d) {
+    return d;
+  }); 
 
-    // Domain is set to 100 because it's a percentage
-    let scale = d3
-      .scaleLinear()
-      .domain([0, 100])
-      // Mapping the range to be the circumference
-      .range([0, 2 * PI]);
+// Domain is set to 100 because it's a percentage
+let scale = d3
+  .scaleLinear()
+  .domain([0, 100])
+  // Mapping the range to be the circumference
+  .range([0, 2 * PI]);
 
-    // This slices the array so the final 100% label isn't shown (as it will overlap with 0%)
-    let ticks = scale.ticks(numTicks).slice(0, -1);
-    // Setting the unique keys to be the name of the quartile
+// This slices the array so the final 100% label isn't shown (as it will overlap with 0%)
+let ticks = scale.ticks(numTicks).slice(0, -1);
 
-    const numArcs = 4;
+// Setting the unique keys to be the name of the quartile
+const numArcs = 4;
 
-    // Calculating the width of an arc, taking into account the number of arcs, the padding, the overall chart radius and
-    // The minimum radius (of center donut)
-    const arcWidth =
-      (chartRadius - arcMinRadius - numArcs * arcPadding) / numArcs;
+// Calculating the width of an arc, taking into account the number of arcs, the padding, the overall chart radius and
+// The minimum radius (of center donut)
+const arcWidth = (chartRadius - arcMinRadius - numArcs * arcPadding) / numArcs;
 
-    let arc = d3
-      .arc()
-      .innerRadius((d, i) => getInnerRadius(i))
-      .outerRadius((d, i) => getOuterRadius(i))
-      .startAngle(0)
-      .endAngle((d, i) => scale(d));
+// Creating the arc and inner/outer radius so i's a donut
+let arc = d3
+  .arc()
+  .innerRadius((d, i) => getInnerRadius(i))
+  .outerRadius((d, i) => getOuterRadius(i))
+  .startAngle(0)
+  .endAngle((d, i) => scale(d));
 
+  // Converts the radians to degrees
+function rad2deg(angle) {
+  return (angle * 180) / PI;
+}
 
-      function rad2deg(angle) {
-        return (angle * 180) / PI;
-      }
-  
-      function getInnerRadius(index) {
-        return arcMinRadius + (numArcs - (index + 1)) * (arcWidth + arcPadding);
-      }
-  
-      function getOuterRadius(index) {
-        return getInnerRadius(index) + arcWidth;
-      }
-  
+// Calculates the inner radius
+function getInnerRadius(index) {
+  return arcMinRadius + (numArcs - (index + 1)) * (arcWidth + arcPadding);
+}
 
+// Calculates the outer radius
+function getOuterRadius(index) {
+  return getInnerRadius(index) + arcWidth;
+}
 
-updatePieChart("GB Group PLC")
+// Initial company to be loaded
+updatePieChart("GB Group PLC");
 
-function updatePieChart(company){
-  d3.selectAll('path').exit().remove()
+// To update the pie chart
+function updatePieChart(company) {
+  d3.selectAll("path").exit().remove();
   // Reading the csv file
   d3.csv(payGapData).then(function (data) {
-
-
     // Here we filter it to the chosen employer
     data = data.filter((line) => line.EmployerName == company);
 
@@ -128,37 +134,43 @@ function updatePieChart(company){
     // Reversing the array so the lower quartile is the center
     data = data.reverse();
 
+    // This sets the axis (which is a radius line)
     let radialAxis = svg
-    .append("g")
-    .attr("class", "r axis")
-    .selectAll("g")
-    .data(data)
-    .enter()
-    .append("g");
+      .append("g")
+      .attr("class", "r axis")
+      .selectAll("g")
+      .data(data)
+      .enter()
+      .append("g");
 
+    // This is the ring axis
     radialAxis
       .append("text")
       .attr("x", labelPadding)
+      // Positions one line for each 25%
       .attr("y", (d, i) => -getOuterRadius(i) + arcPadding)
       .text((d) => d.name)
       .style("font-size", "10px")
       .attr("font-family", "sans-serif");
 
-      radialAxis
+    // Creates the circle shape for each quartile
+    radialAxis
       .append("circle")
       .attr("r", (d, i) => getOuterRadius(i) + arcPadding);
 
-
-
+    // This is the line axis for each 10%
     let axialAxis = svg
       .append("g")
       .attr("class", "a axis")
       .selectAll("g")
+      // Split into the ticks (10)
       .data(ticks)
       .enter()
       .append("g")
+      // Rotates the line so they're evenly spaced
       .attr("transform", (d) => "rotate(" + (rad2deg(scale(d)) - 90) + ")");
 
+    // Adds the outer line
     axialAxis.append("line").attr("x2", chartRadius);
 
     axialAxis
@@ -180,9 +192,10 @@ function updatePieChart(company){
           (chartRadius + 10) +
           ",0)"
       )
+      // Percentage sign to the label
       .text((d) => d + "%");
 
-    //data arcs
+    //Attaching the data to the arcs
     let arcs = svg
       .append("g")
       .attr("class", "data")
@@ -191,26 +204,28 @@ function updatePieChart(company){
       .enter()
       .append("path")
       .attr("class", "arc")
-      .style("fill", (d, i) => color(i))
+      // Colour is based on theme
+      .style("fill", (d, i) => color(i));
 
+      // Adding a transition to it is smooth
     arcs
       .transition()
+      // So the quartiles move in turn
       .delay((d, i) => i * 200)
       .duration(1000)
       .attrTween("d", arcTween);
 
-
-
+    // Showing/hiding tooltip on mouse event
     arcs.on("mousemove", showTooltip);
     arcs.on("mouseout", hideTooltip);
 
-
-
+    // This interpolates between the two angles for the transition
     function arcTween(d, i) {
       let interpolate = d3.interpolate(0, d.value);
       return (t) => arc(interpolate(t), i);
     }
 
+    // Showing the tooltip
     function showTooltip(event, d) {
       tooltip
         .style("left", event.pageX + 10 + "px")
@@ -219,31 +234,29 @@ function updatePieChart(company){
         .html(d.value + "%");
     }
 
+    // Hiding the tooltip
     function hideTooltip() {
       tooltip.style("display", "none");
     }
+  });
 
+  // When the button is changed, run the updateChart function
+  d3.select("#selectButton").on("change", function (d) {
+    function arcTween(d, i) {
+      let interpolate = d3.interpolate(d.value, 0);
+      return (t) => arc(interpolate(t), i);
+    }
 
-  })
-
-    // When the button is changed, run the updateChart function
-    d3.select("#selectButton").on("change", function(d) {
-      
-      function arcTween(d, i) {
-        let interpolate = d3.interpolate(d.value, 0);
-        return (t) => arc(interpolate(t), i);
-      }
-  
-      d3.selectAll('path.arc')
+    d3.selectAll("path.arc")
+    // Adding a transition when the arcs are removed
       .transition()
       .delay((d, i) => i * 200)
       .duration(1000)
       .attrTween("d", arcTween)
-      .remove()
-      // console.log(hi)
-      // recover the option that has been chosen
-      var company = d3.select(this).property("value")
-      // run the updateChart function with this selected option
-      updatePieChart(company)
-  })
+      .remove();
+    // Get the option form the dropdown
+    var company = d3.select(this).property("value");
+    // Run the updateChart function with the choice
+    updatePieChart(company);
+  });
 }
